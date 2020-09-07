a093d8ec479f77e0189ff26fe15322412a465a75
Michael Bord <5843954+michaelbord@users.noreply.github.com>
2020-09-07 21:40:54 +0200

    Ajout des fichiers

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace mars_attack
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GetData(1);
+            GetData(2);
+            GetData(3);
+
+            Pause();
+
+        }
+
+        private static void Pause()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press a key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void GetData(int nbResult)
+        {
+            Console.WriteLine($"Nb de résultat attendu: {nbResult}");
+
+            using SqlConnection sqlConnection = new SqlConnection("Server=localhost; Database=AdventureWorks2016; User Id=mars_attack_user; Password=film2merde; MultipleActiveResultSets=true");
+            using SqlCommand sqlCommand = new SqlCommand("p_get_mars_attack", sqlConnection);
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("nb_result", nbResult);
+            sqlConnection.Open();
+
+            using SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+
+            int result = 1;
+            do
+            {
+
+                int line = 0;
+                while (sqlReader.Read())
+                {
+                    Console.Write($"{result}|{++line}: ");
+
+                    for (int i = 0; i < sqlReader.FieldCount; i++)
+                    {
+                        Console.Write($"{sqlReader[i]}\t");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                result++;
+            } while (sqlReader.NextResult());
+        }
+    }
+}
---
+++ sql/dbo_p_get_mars_attack.sql
@@ -0,0 +1,54 @@
+
+
+USE [master]
+GO
+
+
+CREATE LOGIN [mars_attack_user]
+	WITH PASSWORD = N'film2merde'
+		,DEFAULT_DATABASE = [AdventureWorks2016]
+		,DEFAULT_LANGUAGE = [us_english]
+		,CHECK_EXPIRATION = OFF
+		,CHECK_POLICY = OFF
+GO
+
+CREATE USER [mars_attack_user] FOR LOGIN [mars_attack_user]
+GO
+
+USE AdventureWorks2016
+GO
+
+/*
+EXEC p_get_mars_attack;
+EXEC p_get_mars_attack @nb_result = 2;
+EXEC p_get_mars_attack @nb_result = 3;
+*/
+CREATE
+	OR
+
+ALTER PROCEDURE p_get_mars_attack (@nb_result TINYINT = 1)
+AS
+BEGIN
+	SELECT TOP 10 AddressID
+		,AddressLine1
+		,AddressLine2
+		,City
+	FROM [Person].[Address];
+
+	IF @nb_result > 1
+	BEGIN
+		SELECT TOP 10 BusinessEntityID
+			,PersonType
+			,NameStyle
+		FROM [Person].[Person];
+	END
+
+	IF @nb_result > 2
+	BEGIN
+		SELECT TOP 10 ProductID
+			,Name
+		FROM [Production].[Product]
+	END
+END
+
+GRANT EXECUTE ON p_get_mars_attack TO [mars_attack_user];
