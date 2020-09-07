b00638ebe4285f1444819478c18258328fdc4dcc
Michael Bord <5843954+michaelbord@users.noreply.github.com>
2020-09-07 21:57:08 +0200

    Ajout de commentaires

--- Program.cs
+++ Program.cs
@@ -7,12 +7,16 @@ namespace mars_attack
     {
         private static void Main(string[] args)
         {
+            // GetDate with 1 result
             GetData(1);
+
+            // GetDate with 2 results
             GetData(2);
+
+            // GetDate with 3 result
             GetData(3);
 
             Pause();
-
         }
 
         private static void Pause()
@@ -25,6 +29,7 @@ namespace mars_attack
         private static void GetData(int nbResult)
         {
             Console.WriteLine($"Nb de résultat attendu: {nbResult}");
+            Console.WriteLine("======================================");
 
             using SqlConnection sqlConnection = new SqlConnection("Server=localhost; Database=AdventureWorks2016; User Id=mars_attack_user; Password=film2merde; MultipleActiveResultSets=true");
             using SqlCommand sqlCommand = new SqlCommand("p_get_mars_attack", sqlConnection);
@@ -35,14 +40,18 @@ namespace mars_attack
             using SqlDataReader sqlReader = sqlCommand.ExecuteReader();
 
             int result = 1;
+
+            // Boucle sur les result
             do
             {
-
                 int line = 0;
+
+                // Boucle sur les lignes d'un result
                 while (sqlReader.Read())
                 {
                     Console.Write($"{result}|{++line}: ");
 
+                    // Boucle sur les colonnes
                     for (int i = 0; i < sqlReader.FieldCount; i++)
                     {
                         Console.Write($"{sqlReader[i]}\t");
